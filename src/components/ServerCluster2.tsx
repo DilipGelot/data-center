bd420b77ff5ef15f9366d2d9ecb6f54f68e2ec3a
DilipGelot <dilipgelot16@gmail.com>
2026-04-11 19:51:07 +0530

    feat: add ModelLabel component to ServerCluster2 interactive model

--- src/components/ServerCluster2.tsx
+++ src/components/ServerCluster2.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import ModelLabel from "./ModelLabel";
 import InteractiveModel from "./InteractiveModel";
 
 export default function ServerCluster2() {
   return (
     <div className="absolute top-[59%] left-[56%] -translate-x-1/2 -translate-y-1/2 pointer-events-auto">
       <InteractiveModel id="ServerCluster2">
+        <ModelLabel label="Liquid Cooled Rack and Servers" />
         <Image
           src="/assets/servers.webp"
           alt="Rack and Servers"
